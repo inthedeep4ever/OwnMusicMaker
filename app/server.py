f96bbde4998024d2803820d3164117c5f647c652
hollowearthyes <connect@wecare-fortech.com>
2025-11-27 11:05:59 +0100

    new

--- app/server.py
+++ app/server.py
@@ -1,29 +1,23 @@
 from fastapi import FastAPI, UploadFile
 import shutil
 import os
-from generate import *
-
 
 app = FastAPI()
-
 
 UPLOAD_DIR = "uploads"
 GENERATED_DIR = "generated"
 
-
 @app.post("/api/upload")
 def upload_wav(file: UploadFile):
-filepath = f"{UPLOAD_DIR}/{file.filename}"
-with open(filepath, "wb") as buffer:
-shutil.copyfileobj(file.file, buffer)
-return {"status": "uploaded", "file": file.filename}
+    # Let op 4 spaties inspringing
+    filepath = f"{UPLOAD_DIR}/{file.filename}"
+    with open(filepath, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"status": "uploaded", "file": file.filename}
 
 
 @app.get("/api/generate")
 def generate():
-os.system("python generate.py")
-return {"status": "done", "file": "generated/output.wav"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8000)
+    # Alles ingesprongen binnen de functie
+    os.system("python generate.py")
+    return {"status": "done", "file": f"{GENERATED_DIR}/output.wav"}
